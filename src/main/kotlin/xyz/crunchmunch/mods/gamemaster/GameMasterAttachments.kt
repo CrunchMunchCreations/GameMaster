4aa2d92e1fa08f8ca015a8ffa0f128def86a284d
Naz <bluspringindohq@gmail.com>
2025-11-22 13:14:20 +0800

    Add custom attachment to allow associating a marker with an entity

--- src/main/kotlin/xyz/crunchmunch/mods/gamemaster/GameMasterAttachments.kt
+++ src/main/kotlin/xyz/crunchmunch/mods/gamemaster/GameMasterAttachments.kt
@@ -2,9 +2,14 @@ package xyz.crunchmunch.mods.gamemaster
 
 import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry
 import net.fabricmc.fabric.api.attachment.v1.AttachmentType
+import net.minecraft.core.UUIDUtil
+import java.util.*
 
 object GameMasterAttachments {
     @JvmField val HAS_PASSENGERS: AttachmentType<Unit> = AttachmentRegistry.create(GameMaster.id("has_passengers"))
+    @JvmField val ASSOCIATED_MARKER: AttachmentType<UUID> = AttachmentRegistry.create(GameMaster.id("associated_marker")) {
+        it.persistent(UUIDUtil.CODEC)
+    }
 
     fun init() {}
 }
