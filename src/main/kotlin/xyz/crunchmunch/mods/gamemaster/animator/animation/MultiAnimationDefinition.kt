c93b9255b44c38bc13584cc789e159c2cda21524
Naz <bluspringindohq@gmail.com>
2025-11-22 17:39:27 +0800

    Fix codec load order

--- src/main/kotlin/xyz/crunchmunch/mods/gamemaster/animator/animation/MultiAnimationDefinition.kt
+++ src/main/kotlin/xyz/crunchmunch/mods/gamemaster/animator/animation/MultiAnimationDefinition.kt
@@ -8,13 +8,13 @@ abstract class MultiAnimationDefinition {
     abstract val type: AnimationDefinitionType<*>
 
     companion object {
+        val TICKS_AS_SECONDS_STRING_CODEC: Codec<Int> = Codec.STRING.xmap({ (it.toFloat() * 20).toInt() }, { (it.toFloat() / 20f).toString() })
+        val TICKS_AS_SECONDS_FLOAT_CODEC: Codec<Int> = Codec.FLOAT.xmap({ (it.toFloat() * 20).toInt() }, { (it.toFloat() / 20f) })
+
         val CODEC: Codec<MultiAnimationDefinition> = Codec.withAlternative(
             AnimatableManager.ANIMATION_TYPE_REGISTRY.byNameCodec()
                 .dispatch("type", MultiAnimationDefinition::type, AnimationDefinitionType<*>::codec),
             BedrockMultiAnimationDefinition.CODEC.codec()
         )
-
-        val TICKS_AS_SECONDS_STRING_CODEC: Codec<Int> = Codec.STRING.xmap({ (it.toFloat() * 20).toInt() }, { (it.toFloat() / 20f).toString() })
-        val TICKS_AS_SECONDS_FLOAT_CODEC: Codec<Int> = Codec.FLOAT.xmap({ (it.toFloat() * 20).toInt() }, { (it.toFloat() / 20f) })
     }
 }
