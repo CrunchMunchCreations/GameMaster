17d4fa3988a5858863ae24c29fec7d399973fb11
Naz <bluspringindohq@gmail.com>
2025-11-23 16:21:57 +0800

    Make a swap entity command

--- src/main/kotlin/xyz/crunchmunch/mods/gamemaster/commands/GameMarkerCommands.kt
+++ src/main/kotlin/xyz/crunchmunch/mods/gamemaster/commands/GameMarkerCommands.kt
@@ -2,16 +2,23 @@ package xyz.crunchmunch.mods.gamemaster.commands
 
 import de.phyrone.brig.wrapper.DSLCommandNode
 import de.phyrone.brig.wrapper.executesNoResult
+import net.minecraft.commands.CommandBuildContext
 import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.SharedSuggestionProvider
-import net.minecraft.commands.arguments.DimensionArgument
-import net.minecraft.commands.arguments.ResourceLocationArgument
+import net.minecraft.commands.arguments.*
+import net.minecraft.core.registries.Registries
 import net.minecraft.network.chat.ClickEvent
 import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.HoverEvent
+import net.minecraft.util.ProblemReporter
+import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.EntitySpawnReason
+import net.minecraft.world.level.storage.TagValueInput
+import net.minecraft.world.level.storage.TagValueOutput
 import xyz.crunchmunch.mods.gamemaster.game.marker.GameMarkerManager
+import xyz.crunchmunch.mods.gamemaster.utils.sendSuccess
 
-fun DSLCommandNode<CommandSourceStack>.gameMarkerCommands() {
+fun DSLCommandNode<CommandSourceStack>.gameMarkerCommands(buildCtx: CommandBuildContext) {
     literal("markers") {
         literal("count") {
             argument("type", ResourceLocationArgument.id()) {
@@ -113,6 +120,33 @@ fun DSLCommandNode<CommandSourceStack>.gameMarkerCommands() {
                         }
 
                     markers.size
+                }
+            }
+        }
+
+        literal("swapentity") {
+            argument("entities", EntityArgument.entities()) {
+                argument("entity_type", ResourceKeyArgument.key(Registries.ENTITY_TYPE)) {
+                    executesNoResult { ctx ->
+                        val entities = EntityArgument.getEntities(ctx, "entities")
+                        val entityType = ResourceArgument.getEntityType(ctx, "entity_type")
+
+                        for (entity in entities) {
+                            val newEntity = entityType.value().create(entity.level(), EntitySpawnReason.CONVERSION)
+                                ?: throw IllegalStateException("Failed to create replacement entity for ${entity.uuid}!")
+
+                            newEntity.snapTo(entity.position())
+
+                            val value = TagValueOutput.createWithContext(ProblemReporter.DISCARDING, ctx.source.registryAccess())
+                            entity.saveWithoutId(value)
+                            entity.remove(Entity.RemovalReason.DISCARDED)
+                            newEntity.load(TagValueInput.create(ProblemReporter.DISCARDING, ctx.source.registryAccess(), value.buildResult()))
+
+                            entity.level().addFreshEntity(newEntity)
+                        }
+
+                        ctx.source.sendSuccess(Component.literal("Swapped ${entities.size} entities to ${entityType.registeredName}"), true)
+                    }
                 }
             }
         }
