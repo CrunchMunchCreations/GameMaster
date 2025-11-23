8ce841a4746e5dbcf41d71bfdaf77f0c20c8fe33
Naz <bluspringindohq@gmail.com>
2025-11-23 20:15:37 +0800

    Make BlockArea iterable

--- src/main/kotlin/xyz/crunchmunch/mods/gamemaster/utils/serializable/BlockArea.kt
+++ src/main/kotlin/xyz/crunchmunch/mods/gamemaster/utils/serializable/BlockArea.kt
@@ -7,13 +7,17 @@ import net.minecraft.world.phys.AABB
 data class BlockArea(
     val from: BlockPos,
     val to: BlockPos
-) {
+) : Iterable<BlockPos> {
     fun getBoundingBox(): AABB {
         return AABB(from.center, to.center).inflate(1.05)
     }
 
     override fun toString(): String {
         return "${from.toShortString()}, ${to.toShortString()}"
+    }
+
+    override fun iterator(): Iterator<BlockPos> {
+        return BlockPos.betweenClosed(this.from, this.to).iterator()
     }
 
     companion object {
